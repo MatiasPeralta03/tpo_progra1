8bcfc976b3543d3ac30668b3ebdcc80473c45638
Bruno <bruno.gab.galvan@gmail.com>
2024-08-24 16:30:56 -0300

    ejer 02

--- tps/02/02.py
+++ tps/02/02.py
@@ -15,7 +15,39 @@ def generar_lista_random(lenght):
         lst.append(rd.randint(1,100))
     return lst
 
+def elemento_repetido(lst):
+    rep = False
+    index = 0
+    while rep == False and index != (len(lst) - 1):
+        if lst.count(lst[index]) >= 2:
+            rep = True
+        index = index + 1
+
+#    Otra forma menos optima
+#    for value in lst:
+#        if lst.count(value) >= 2:
+#            rep = True
+
+    return rep
+             
+def depurar_elementos_unicos(lst):
+    for value in lst:
+        if lst.count(value) >= 2:
+            print("Valor %d repetido %d veces" %(value,lst.count(value)))
+            while lst.count(value) != 1:
+                lst.remove(value)
+    return lst
+
 num = int(input("Ingrese el valor de numeros que desea en la lista: "))
 
-print("Lista:", generar_lista_random(num))
+lista = generar_lista_random(num)
+
+print("Lista sin depurar:", lista)
+
+if elemento_repetido(lista):
+    print("Lista depurada:", depurar_elementos_unicos(lista))
+else:
+    print("No posee elementos repetidos.")
+
+
 
