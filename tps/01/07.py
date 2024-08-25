86f9fe768346da9481694ef4f4d6365f7b2d0ee8
Bruno <bruno.gab.galvan@gmail.com>
2024-08-24 21:53:51 -0300

    update

--- tps/01/07.py
+++ tps/01/07.py
@@ -10,10 +10,10 @@ def validarFecha(day, month, year):
     valid_date = False
 
     if 1582 < year:
-        if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+        if month in [1,3,5,7,8,10,12]:
             if 0 < day and day <= 31:
                 valid_date = True
-        elif month == 4 or month == 6 or month == 9 or month == 11:
+        elif month in [4,6,9,11]:
             if 0 < day and day <= 30:
                 valid_date = True
         elif month == 2:
@@ -37,7 +37,7 @@ def validarFecha(day, month, year):
     return valid_date
 
 def unDiaMas(day, month, year):
-    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+    if month in [1,3,5,7,8,10,12]:
         if day == 31:
             day = 1
             if month == 12:
@@ -47,7 +47,7 @@ def unDiaMas(day, month, year):
                 month = month + 1
         else:
             day = day + 1
-    elif month == 4 or month == 6 or month == 9 or month == 11:
+    elif month in [4,6,9,11]:
         if day == 30:
             day = 1
             month = month + 1
